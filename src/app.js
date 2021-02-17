756b2c0f2d8f6ee7be9681c440a7ab3beb45f996
DBaryga <baryga.priv@gmail.com>
2021-02-17 12:42:30 +0100

    working base

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import slider from './components/slider.vue';
+
+new Vue({
+    el: '#master_vue',
+    components:{
+        slider
+    }
+})
---
+++ src/components/slider.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="yellow-thing">
+      <p>Vue component</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "slider",
+    mounted() {
+      console.log('test')
+    }
+  }
+</script>
